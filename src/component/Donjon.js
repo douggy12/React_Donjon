8ef3bc1943c8aaa77edaa50440d9cd38971943d2
Douglas Metthey <d.metthey@gmail.com>
2019-07-07 17:39:56 +0200

    <OPTI> Suppression ncomponent Etage

--- src/component/Donjon.js
+++ src/component/Donjon.js
@@ -1,54 +1,97 @@
 import React from 'react';
-import Etage from './Etage';
 import Utils from '../utils/Utils';
 
 class Donjon extends React.Component {
     constructor(props) {
-        
+
         super(props);
         this.state = {
             donjon: props.donjon
-        } 
+        }
         console.log("donjon:construct");
     }
-    
 
-    ouvrirPorte = (index) => {
+
+    ouvrirPorte(index) {
         let donjon = this.state.donjon;
-        Utils.last(Utils.last(donjon.etages).couloirs).portes[index] = {status : "open"};
+        Utils.last(Utils.last(donjon.etages).couloirs).portes[index] = { status: "open" };
         this.setState({
             donjon: donjon
         })
     }
 
-    explorer = () => {
+    explorer() {
         let donjon = this.state.donjon;
         Utils.last(donjon.etages).couloirs.push(Utils.getEmptyHall(4));
-        this.setState({donjon: donjon});
-        
+        this.setState({ donjon: donjon });
+
     }
 
-    descendre(){
+    rentrer(index) {
+        let donjon = this.props.donjon;
+        Utils.last(Utils.last(donjon.etages).couloirs).portes[index].status = "fight"
+        this.props.move("Salle", this.props.donjon);
+    }
+
+    descendre() {
         let donjon = this.state.donjon;
         donjon.etages.push({
-            couloirs:[Utils.getEmptyHall(4)]
+            couloirs: [Utils.getEmptyHall(4)]
         });
-    
+
         this.setState({
-            donjon : donjon
+            donjon: donjon
         })
     }
 
+    renderEtage(portes) {
+        const etage = portes.map((porte, index) => {
+            if (porte.status === "open") {
+
+                return <button key={index} className="opened-door" onClick={() => {
+                    this.rentrer(index);
+                }}>Monstre</button>
+            } else if (porte.status === "close") {
+
+                return (
+                    <button key={index} className="door" onClick={() => {
+                        this.ouvrirPorte(index);
+                    }
+                    }>
+
+                    </button>
+                );
+            } else if (porte.status === "defeat") {
+                return (
+                    <div key={index} className="defeat">
+                        X
+                </div>
+                );
+            } else {
+                return null;
+            }
+        });
+        return etage;
+    }
+
     render() {
+        const temps = (Utils.last(this.props.donjon.etages).couloirs.length - 1) * 4;
+        const isExplored = temps === 24;
+        const etage = this.renderEtage(Utils.last(Utils.last(this.props.donjon.etages).couloirs).portes)
+
+
         return (
             <div className='donjon'>
                 <h2>Etage -{this.state.donjon.etages.length}</h2>
-                <Etage 
-                    move={this.props.move}
-                    explorer={this.explorer}
-                    openDoor = {this.ouvrirPorte} 
-                    donjon ={this.props.donjon}
-                />
+                <div className="temps">
+                    {temps}/24
+                </div>
+                <div className="etage">
+                    {etage}
+                </div>
+
+                {!isExplored && <button onClick={() => this.explorer()}>Explorer</button>}
+
                 <button onClick={() => this.descendre()}>Descendre</button>
             </div>
         );
