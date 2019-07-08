aeb2a053d61cd544e0f29ed635a45b61311604ad
Douglas Metthey <d.metthey@gmail.com>
2019-07-08 02:01:45 +0200

    EVOL<integration IHM hero>

--- src/component/Salle.js
+++ src/component/Salle.js
@@ -3,19 +3,68 @@ import Utils from '../utils/Utils';
 
 
 class Salle extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            donjon: props.donjon,
+            hero: props.hero
+        }
+    }
+
     gagner() {
         Utils.last(Utils.last(this.props.donjon.etages).couloirs).portes.find(porte => porte.status === 'fight').status = 'defeat';
-        return this.props.move("Donjon",this.props.donjon);
+        return this.props.move("Donjon", this.props.donjon);
+    }
+
+    renderSante() {
+        let barreSante = [];
+        for (let i = 0; i < this.state.hero.sante; i++) {
+            barreSante.push(<div>❤</div>);
+        }
+        return barreSante;
+    }
+
+    renderStockDes(nbDes) {
+        let des = [];
+        for (let i = 0; i < nbDes; i++) {
+            des.push(<div className="de"></div>);
+        }
+        return des;
     }
 
     render() {
-       
+
         return (
-            <div className = "salle">
+            <div className="salle">
+
                 <div>Combattre le Monstre</div>
-                <button onClick={()=>this.gagner()}>Gagné</button>
-                <button onClick={()=> this.props.move("Mort",this.props.donjon)}>Perdu</button>
+                <div className="combat">
+                    <div className="hero">
+                        <div className="topbar">
+                            <div className="nom">{this.state.hero.name}</div>
+                            <div className="sante">{this.renderSante()}</div>
+                        </div>
+                        <div className="stats">
+                            <div className = "force">
+                                <div className="titre">Force</div>
+                                <div className = "stock-des">{this.renderStockDes(this.state.hero.force)}</div>
+                            </div>
+                            <div className = "agilite">
+                                <div className="titre">Agilite</div>
+                                <div className = "stock-des">{this.renderStockDes(this.state.hero.agilite)}</div>
+                            </div>
+                            <div className = "magie">
+                                <div className="titre">Magie</div>
+                                <div className = "stock-des">{this.renderStockDes(this.state.hero.magie)}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="monstre">
+                        Monstre
+                    </div>
+                </div>
+                <button onClick={() => this.gagner()}>Gagné</button>
+                <button onClick={() => this.props.move("Mort", this.props.donjon)}>Perdu</button>
             </div>
         );
     }
---
+++ src/heros/Hero.js
@@ -0,0 +1,8 @@
+class Hero{
+    name="BrakinBaad";
+    sante = 4;
+    force = 3;
+    agilite = 2;
+    magie = 2;
+}
+export default Hero;
