431fad21f3c316ead51e4d957307a02edaa5130e
Douglas Metthey <d.metthey@gmail.com>
2019-07-16 02:18:24 +0200

    OPTI<style button>

--- src/component/Salle.js
+++ src/component/Salle.js
@@ -18,11 +18,11 @@ class Salle extends React.Component {
             monstre: {
                 nom: "Rat affamé",
                 stats: [
-                    { type: "force", value : 2, multi:false,required:true,damages:[]},
-                    { type: "force",  value: 3, multi:false,damages:[1,1] },
-                    { type: "agilite", value: 2, multi:true,damages:[null,1] }
+                    { type: "force", value: 2, multi: false, required: true, damages: [] },
+                    { type: "force", value: 3, multi: false, damages: [1, 1] },
+                    { type: "agilite", value: 2, multi: true, damages: [null, 1] }
                 ],
-                requiredDone:false,
+                requiredDone: false,
             },
             selectedDice: { type: null, index: null },
             isDicesRolled: false,
@@ -65,34 +65,34 @@ class Salle extends React.Component {
         return this.props.move("Donjon", this.state.donjon, this.state.hero);
     }
     perdre() {
-        
-        if(this.state.hero.sante > 0) {
+
+        if (this.state.hero.sante > 0) {
             let donjon = this.state.donjon;
             this.calculateDamage();
 
             Utils.last(Utils.last(this.props.donjon.etages).couloirs).portes.find(porte => porte.status === 'fight').status = 'open';
-            this.setState({donjon:donjon});
+            this.setState({ donjon: donjon });
             this.props.move("Donjon", this.state.donjon, this.state.hero);
         }
-        if(this.state.hero.sante < 1){
-                this.props.move("Mort", this.state.donjon, this.state.hero);
+        if (this.state.hero.sante < 1) {
+            this.props.move("Mort", this.state.donjon, this.state.hero);
         }
-        
+
     }
 
-    calculateDamage(){
+    calculateDamage() {
         let hero = this.state.hero;
         let donjon = this.state.donjon;
-        this.state.monstre.stats.forEach((stat)=>{
-            if(stat.value > 0){
-                if(stat.damages.length > 0){
+        this.state.monstre.stats.forEach((stat) => {
+            if (stat.value > 0) {
+                if (stat.damages.length > 0) {
                     hero.damage += stat.damages[0];
                     donjon.temps += stat.damages[1];
                 }
-               
+
             }
         });
-        this.setState({hero:hero,donjon:donjon});
+        this.setState({ hero: hero, donjon: donjon });
     }
 
     renderStockDes(listDe, type) {
@@ -104,32 +104,32 @@ class Salle extends React.Component {
         />);
     }
 
-    isRequiredDone(monstre){
+    isRequiredDone(monstre) {
         const requArray = monstre.stats.filter(stat => stat.required);
-        let result = requArray.every((stat)=>{
-            if(stat.value > 0){    
+        let result = requArray.every((stat) => {
+            if (stat.value > 0) {
                 return false;
             }
-            return true;      
+            return true;
         })
         return result;
     }
     onDrop = (item, target) => {
 
         let stockDe = this.state.stockDe;
-        
+
         let monstre = this.state.monstre;
-        if(item.stat.value >= target.stat.value){
-            monstre.stats[target.index].value=0;
-        }else if (target.stat.multi){
+        if (item.stat.value >= target.stat.value) {
+            monstre.stats[target.index].value = 0;
+        } else if (target.stat.multi) {
             let result = target.stat.value - item.stat.value;
-            monstre.stats[target.index].value = result < 0 ? 0:result;
+            monstre.stats[target.index].value = result < 0 ? 0 : result;
         }
-        if (this.isRequiredDone(monstre)){
+        if (this.isRequiredDone(monstre)) {
             monstre.requiredDone = true;
         }
         stockDe[item.type].splice([item.id], 1);
-       
+
         this.setState({
             stockDe: stockDe,
             monstre: monstre
@@ -161,18 +161,23 @@ class Salle extends React.Component {
                     <div className="combat">
                         <div className="hero">
                             <Hero hero={this.state.hero} />
-                            <div className="stats">
-                                <div className="force">
-                                    <div className="titre">Force</div>
-                                    <div className="stock-des">{this.renderStockDes(this.state.stockDe.force, "force")}</div>
+                            <div className="action">
+                                <div className="stats">
+                                    <div className="force">
+                                        <div className="titre">Force</div>
+                                        <div className="stock-des">{this.renderStockDes(this.state.stockDe.force, "force")}</div>
+                                    </div>
+                                    <div className="agilite">
+                                        <div className="titre">Agilite</div>
+                                        <div className="stock-des">{this.renderStockDes(this.state.stockDe.agilite, "agilite")}</div>
+                                    </div>
+                                    <div className="magie">
+                                        <div className="titre">Magie</div>
+                                        <div className="stock-des">{this.renderStockDes(this.state.stockDe.magie, "magie")}</div>
+                                    </div>
                                 </div>
-                                <div className="agilite">
-                                    <div className="titre">Agilite</div>
-                                    <div className="stock-des">{this.renderStockDes(this.state.stockDe.agilite, "agilite")}</div>
-                                </div>
-                                <div className="magie">
-                                    <div className="titre">Magie</div>
-                                    <div className="stock-des">{this.renderStockDes(this.state.stockDe.magie, "magie")}</div>
+                                <div className='roll'>
+                                    <button onClick={() => this.rollDices()}>Roll The Dice !</button>
                                 </div>
                             </div>
                         </div>
@@ -187,7 +192,7 @@ class Salle extends React.Component {
                         </div>
                     </div>
                 </DndProvider>
-                <button onClick={() => this.rollDices()}>Roll The Dice !</button>
+
                 <div>
                     {this.isGagne() && <button onClick={() => this.gagner()}>Gagné</button>}
                     <button onClick={() => this.perdre()}>Perdu</button>
