1072c70c1b64d148215cac75d2ac384c4889c775
Douglas Metthey <d.metthey@gmail.com>
2019-08-01 23:14:22 +0200

    EVO<Affichage item>

--- src/component/Item.js
+++ src/component/Item.js
@@ -3,11 +3,13 @@ import React from 'react';
 class Item extends React.Component{
     renderStats(){
         let stats=[];
-        for (const stat in this.props.item.stats) {
-            if (this.props.item.stats.hasOwnProperty(stat)) {
+        for (const stat in this.props.item.item.stats) {
+            let key = 0;
+            if (this.props.item.item.stats.hasOwnProperty(stat)) {
                 stats.push(
-                    <div className = {stat}>{this.props.item.stats[stat]}</div>
-                );                
+                    <div key = {key} className = {stat}> + {this.props.item.item.stats[stat]} {stat}</div>
+                ); 
+                key ++;               
             }
         }
         return(
@@ -16,17 +18,20 @@ class Item extends React.Component{
 
     }
     renderInfo(){
+        
         return(
-            <div className='stats'>
-                <div className='nom'>{this.props.item.nom}</div>
+            <div className='box'>
+                <div className='nom'>{this.props.item.item.nom}</div>
                 {this.renderStats()}
             </div>
         )
     }
     render(){
+       
         return(
-            <div className='item' onMouseOver={()=>this.renderInfo()}>
-                <img src={this.props.item.image} alt={this.props.item.nom} />
+            <div className='item'>
+                <img src={this.props.item.item.image} alt={this.props.item.item.nom} />
+                {this.renderInfo()}
             </div>
         );
     }
