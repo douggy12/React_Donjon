b12f16d567522ca066c5f1d08085cf684092bd5a
Douglas Metthey <d.metthey@gmail.com>
2019-08-03 23:39:19 +0200

    EVO<bulle d'info hover sur item>

--- src/component/Item.js
+++ src/component/Item.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class Item extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            hover:false
+        }
+    }
+    onHover(){
+        this.setState({hover:true});
+    }
+    HoverOut(){
+        this.setState({hover:false});
+    }
     renderStats(){
         let stats=[];
         for (const stat in this.props.item.stats) {
             let key = 0;
             if (this.props.item.stats.hasOwnProperty(stat)) {
                 stats.push(
-                    <div key = {key} className = {stat}> + {this.props.item.stats[stat]} {stat}</div>
+                    <div key = {key} className = {stat}> + {this.props.item.stats[stat]}</div>
                 ); 
                 key ++;               
             }
@@ -20,7 +32,7 @@ class Item extends React.Component{
     renderInfo(){
         
         return(
-            <div className='box'>
+            <div className='info'>
                 <div className='nom'>{this.props.item.nom}</div>
                 {this.renderStats()}
             </div>
@@ -31,13 +43,21 @@ class Item extends React.Component{
         return(
             <div className='item'>
                 <img src={this.props.item.image} alt={this.props.item.nom} />
-                {this.renderInfo()}
+                <div className="box">
+                    {this.renderInfo()}
+                </div>
+               
             </div>
         );
        }else if(this.props.render === "mini"){
+           const tooltypeStyle = this.state.hover ? 'block':'none';
            return(
-              <div className='item'>
-                <img src={this.props.item.image} alt={this.props.item.nom} />
+              <div className='item' onMouseOver={()=>this.onHover()} onMouseOut={()=>this.HoverOut()}>
+                <img src={this.props.item.image} alt={this.props.item.nom} />               
+                <div className="mini box" style={{display:tooltypeStyle}}>
+                    {this.renderInfo()}
+                </div>
+                
             </div> 
            );
        }
